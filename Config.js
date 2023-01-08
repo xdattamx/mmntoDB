193bdbf27457cc0a4a23206fb34bfd00aaa7c98c
xdattamx <xsokol@gmail.com>
2023-01-07 19:08:14 -0600

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,3 +3,5 @@ libByName("Config");
 ConfigEntry = Config.findById(id); //search an entry in Spp by ID
 var test = "fp1";
 entryDefault().set(gui,guid());
+var conf = libByName("Config")
+var CallId = conf[0].field("id");
