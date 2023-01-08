3b104215475976f01cd6dd28e6c54e9a57ccb39a
xdattamx <xsokol@gmail.com>
2023-01-07 19:17:19 -0600

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,7 +1,7 @@
 // libByName("Config");
 //ConfigEntry = Config.findById(id); //search an entry in Spp by ID
 // var test = "fp1";
-entryDefault().set(gui,guid());
+entryDefault().set("gui",guid());
 var conf = libByName("Config");
 var CallId = conf[0].field("id");
 //Mensaje de aviso opcional
