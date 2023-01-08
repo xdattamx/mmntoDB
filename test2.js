6a1211765ecc26f4442fd425ec0aecfc0acede8b
xdattamx <xsokol@gmail.com>
2023-01-07 20:47:11 -0600

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,7 @@
+ar e = entrada();
+var libreria = libByName( "config" );
+var id = libreria.field( "id" );
+message(id);
+// var id = e.field( "ID" );
+// var SfEntry = Sf.findById(id);
+// Buscar si la pieza existe en la base de datos Sf por su id
