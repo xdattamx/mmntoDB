54a391a8a36c8fee048e830d270a34e3137a5283
xdattamx <xsokol@gmail.com>
2023-01-29 21:51:21 -0600

    Delete bitly.js

--- bitly.js
+++
@@ -1,4 +0,0 @@
-longurl = https://help.mementodatabase.com/?ht_kb=scripts;
-result = http().get("https://api-ssl.bitly.com/v3/shorten?access_token=%var[1]%&format=txt&longurl= + longurl");
-
-message( result.body  );
