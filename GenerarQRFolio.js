5fcbd3a6293640a53167863250bf30a066de7dec
xdattamx <xsokol@gmail.com>
2025-01-07 22:21:15 -0600

    Update and rename GenerarQRFio.js to GenerarQRFolio.js

--- GenerarQRFolio.js
+++ GenerarQRFolio.js
@@ -1,14 +1,16 @@
 function replaceAll(str, find, replace) { return str.replace(new RegExp(find, 'g'), replace); }
 // Obtener datos de el campo folio
 var a = entry().field("Folio");
+var b = entry().field("cotización");
 // a = replaceAll(a, " ", "%20");
 var toSet = "https://qrcode.tec-it.com/API/QRCode?data=" + a + "Coliza&dpi=150&size=Medium";
 // message(toSet);
 
 // Nueva variable que combina el valor del campo "Folio" con la palabra "Cotiza"
 var combinedValue = a + "Cotiza";
+var c = a + b;
 
-var b = combinedValue;
+// var b = combinedValue;
 // set the field content 
 message(b);
 entry().set("QrFolio", toSet);
