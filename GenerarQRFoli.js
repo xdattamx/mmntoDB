3d515dcabe7feb48e7b5fad021f4e52ec5c6bc5e
xdattamx <xsokol@gmail.com>
2025-01-07 21:45:47 -0600

    Update GenerarQRFoli.js

--- GenerarQRFoli.js
+++ GenerarQRFoli.js
@@ -2,7 +2,7 @@ function replaceAll(str, find, replace) { return str.replace(new RegExp(find, 'g
 // Obtener datos de el campo folio
 var a=entry().field("Folio");
 // a=replaceAll(a," ","%20");
-var toSet= "https://qrcode.tec-it.com/API/QRCode?data="+a+"Coltiza";
+var toSet= "https://qrcode.tec-it.com/API/QRCode?data="+a+"Cotiza&dpi=150&size=Medium";
 message(toSet);
 // set the field content 
 entry().set("QrFolio",toSet);
