b30d881718bc464f99e301fea0c6096a594d6488
xdattamx <xsokol@gmail.com>
2025-01-07 22:30:00 -0600

    Update and rename GenerarQRFolio.js to CotizaQRFolio.js

--- CotizaQRFolio.js
+++ CotizaQRFolio.js
@@ -1,17 +1,13 @@
 function replaceAll(str, find, replace) { return str.replace(new RegExp(find, 'g'), replace); }
 // Obtener datos de el campo folio
 var a = entry().field("Folio");
-var b = entry().field("cotización");
 // a = replaceAll(a, " ", "%20");
-var toSet = "https://qrcode.tec-it.com/API/QRCode?data=" + a + "Coliza&dpi=150&size=Medium";
-// message(toSet);
-
 // Nueva variable que combina el valor del campo "Folio" con la palabra "Cotiza"
 var combinedValue = a + "Cotiza";
-var c = a + b;
-
-// var b = combinedValue;
+var b = combinedValue;
+var toSet = "https://qrcode.tec-it.com/API/QRCode?data=" + b + "&dpi=150&size=Medium";
+// message(toSet);
 // set the field content 
 message(b);
 entry().set("QrFolio", toSet);
-entry().set("barcode", c);
+entry().set("barcode", b);
